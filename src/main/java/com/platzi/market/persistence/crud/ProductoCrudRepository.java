fed87a46954c8ed15abe3af3bf9348c78fe94c01
luisyet <sulcaluis151999@gmail.com>
2023-09-02 21:04:07 -0500

    RestController and RequestMapping annotations

--- src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/platzi/market/persistence/crud/ProductoCrudRepository.java
@@ -9,9 +9,6 @@ import java.util.Optional;
 
 public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
 
-    // @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
-    // List<Producto> getByCategoria(int idCategoria); // No es necesario Integer
-
     List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
     Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
